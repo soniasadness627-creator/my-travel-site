de49511ff75ddbb97ef6a079d136fa8c5bee675f
Admin <soniasadness627@gmail.com>
2026-04-27 11:54:46 +0300

    зміни також )

--- constructor/agent_admin_config.py
+++ constructor/agent_admin_config.py
@@ -5,18 +5,31 @@ from tours.models import (
 )
 
 
-# ========== ПРОСТІ АДМІН-КЛАСИ ДЛЯ ВСІХ МОДЕЛЕЙ ==========
+# ========== АДМІН-КЛАСИ З ОБМЕЖЕННЯМИ ДЛЯ АГЕНТІВ ==========
 
 class TourAdmin(admin.ModelAdmin):
-    list_display = ("title", "country", "price", "start_date")
+    list_display = ("title", "country", "price", "start_date", "author")
     list_filter = ("country", "start_date")
     search_fields = ("title", "country")
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.filter(author=request.user)
+
+    def save_model(self, request, obj, form, change):
+        if not obj.pk:
+            obj.author = request.user
+        super().save_model(request, obj, form, change)
 
 
 class BookingAdmin(admin.ModelAdmin):
     list_display = ('id', 'get_tour_title', 'name', 'phone', 'email', 'created_at')
     list_filter = ('created_at',)
     search_fields = ('name', 'phone', 'email')
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.filter(tour__author=request.user)
 
     def get_tour_title(self, obj):
         return obj.tour.title
@@ -29,11 +42,19 @@ class PriceOptionAdmin(admin.ModelAdmin):
     search_fields = ('tour__title',)
     list_editable = ('price', 'is_available')
 
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.filter(tour__author=request.user)
+
 
 class ReviewAdmin(admin.ModelAdmin):
     list_display = ('id', 'get_tour_title', 'get_author_name', 'rating', 'created_at')
     list_filter = ('rating', 'created_at')
     search_fields = ('comment',)
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.filter(tour__author=request.user)
 
     def get_tour_title(self, obj):
         return obj.tour.title
@@ -51,16 +72,33 @@ class ConsultationAdmin(admin.ModelAdmin):
     list_filter = ('created_at', 'is_processed')
     search_fields = ('name', 'phone', 'comment')
 
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.filter(agent=request.user)
+
 
 class NewsAdmin(admin.ModelAdmin):
     list_display = ("title", "created_at")
     list_filter = ("created_at",)
     search_fields = ("title", "text")
 
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.filter(author=request.user)
+
+    def save_model(self, request, obj, form, change):
+        if not obj.pk:
+            obj.author = request.user
+        super().save_model(request, obj, form, change)
+
 
 class CountryInfoAdmin(admin.ModelAdmin):
     list_display = ('country',)
     search_fields = ('country',)
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs
 
 
 class PriceCalendarAdmin(admin.ModelAdmin):
@@ -68,11 +106,19 @@ class PriceCalendarAdmin(admin.ModelAdmin):
     list_filter = ('tour', 'date')
     search_fields = ('tour__title',)
 
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.filter(tour__author=request.user)
+
 
 class PopularDestinationAdmin(admin.ModelAdmin):
     list_display = ('country', 'order')
     list_editable = ('order',)
     search_fields = ('country',)
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs
 
 
 class TourPriceByTouristsAdmin(admin.ModelAdmin):
@@ -80,6 +126,10 @@ class TourPriceByTouristsAdmin(admin.ModelAdmin):
     list_filter = ('tour', 'adults', 'is_default')
     search_fields = ('tour__title',)
     list_editable = ('price', 'is_default')
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.filter(tour__author=request.user)
 
 
 # ========== РЕЄСТРАЦІЯ ВСІХ МОДЕЛЕЙ ==========
