42ffb67c053b2174d00fbe83ee676e5cae1d77ab
Admin <soniasadness627@gmail.com>
2026-04-27 15:28:35 +0300

    Додано міграцію для створення AgentSite)

--- constructor/migrations/0006_create_agentsite.py
+++ constructor/migrations/0006_create_agentsite.py
@@ -5,27 +5,20 @@ def create_agentsite(apps, schema_editor):
     User = apps.get_model('users', 'User')
     AgentSite = apps.get_model('constructor', 'AgentSite')
 
-    # Знаходимо будь-якого суперадміна
+    # Знаходимо суперадміна
     admin = User.objects.filter(is_superuser=True).first()
-    if admin:
-        if not AgentSite.objects.filter(user=admin).exists():
-            AgentSite.objects.create(
-                user=admin,
-                slug='main-site',
-                agency_name='TourConstructor Agency',
-                show_superadmin_tours=True,
-                primary_color='#086745',
-                secondary_color='#02432c'
-            )
-            print(f"✅ Створено AgentSite для {admin.username}")
-
-
-def reverse_func(apps, schema_editor):
-    pass
+    if admin and not AgentSite.objects.filter(user=admin).exists():
+        AgentSite.objects.create(
+            user=admin,
+            slug='main-site',
+            agency_name='TourConstructor',
+            show_superadmin_tours=True
+        )
+        print(f"✅ Створено AgentSite для {admin.username}")
 
 
 class Migration(migrations.Migration):
     dependencies = [
         ('constructor', '0005_alter_agentsite_primary_color_and_more'),
     ]
-    operations = [migrations.RunPython(create_agentsite, reverse_func)]
+    operations = [migrations.RunPython(create_agentsite)]
