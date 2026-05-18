7f7df7003587ea95fa7f6325fd7f65e8bb9ca2c5
Admin <soniasadness627@gmail.com>
2026-05-18 04:24:41 +0300

    Add

--- constructor/models/blocks.py
+++ constructor/models/blocks.py
@@ -14,6 +14,7 @@ class AgentBlockSettings(models.Model):
         ('tours_from_city', '🏙️ Тури з вашого міста'),
         ('about_us', 'ℹ️ Про нас'),
         ('popular_hotels', '🏨 Популярні готелі'),
+        ('banners', '🎯 Мої банери'),  # якщо потрібен
     ]
 
     POSITION_CHOICES = [
